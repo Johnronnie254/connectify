a05823a609398a3894aa314e5085cb4719ebbb2a
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-11-21 12:06:26 +0300

    added functional button at landing page

--- connectify/src/components/LandingPage.jsx
+++ connectify/src/components/LandingPage.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
-//import ServiceContainer from "./ServicesContainer";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Landingpage.css";
+import Services from "./Services";
 
 const LandingPage = () => {
+  const navigate = useNavigate(); // Use useHistory instead of History
+  function handlePage() {
+    // Renaming the function to follow camelCase convention
+    navigate("/Services");
+  }
   return (
     <div className="containerstyle">
       <h1>Welcome to Connect</h1>
@@ -10,7 +16,7 @@ const LandingPage = () => {
         New in an area? Worry not. We make it easy for you to connect with all
         the service providers in your neighborhood
       </p>
-      <button type="button" className="btn btn-success">
+      <button type="button" className="btn btn-success" onClick={handlePage}>
         Get Our Services
       </button>
     </div>
--- connectify/src/main.jsx
+++ connectify/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client"; // Update import
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root")); // Use createRoot directly
 
 root.render(<App />);
